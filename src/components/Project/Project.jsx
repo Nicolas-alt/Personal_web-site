2313cf407d97f1b0707600865283c4c0ed9c82cf
Nicolas-alt <a10100105555@gmail.com>
2021-07-23 16:18:17 -0500

    fix: tilt effect

--- src/components/Project/Project.jsx
+++ src/components/Project/Project.jsx
@@ -1,5 +1,5 @@
 import PropTypes from 'prop-types'
-import imageProject from '../../assets/img/DvR.png'
+import TiltWrapper from '../TiltWrapper/TiltWrapper'
 import './project.css'
 
 const Project = ({
@@ -8,31 +8,34 @@ const Project = ({
   description,
   styleName,
   textStyleName,
-  nameTec
+  nameTec,
+  imageProject
 }) => {
   return (
-    <article className={styleName}>
-      <div className={textStyleName}>
-        <h4>{typeProject}</h4>
-        <h2>{title}</h2>
-        <p>{description}</p>
-        <a href="https://f.com">View project</a>
-        <div className="div__stackContainer">
-          {nameTec.map(({ name, ico }) => (
-            <div
-              className="div__stack"
-              key={name + ico}
-              style={{ marginRight: '10px' }}
-            >
-              <i className={`bx bxl-${ico}`} />
-              <span>{name}</span>
-            </div>
-          ))}
+    <TiltWrapper>
+      <article className={styleName}>
+        <div className={textStyleName}>
+          <h4>{typeProject}</h4>
+          <h2>{title}</h2>
+          <p>{description}</p>
+          <a href="https://f.com">View project</a>
+          <div className="div__stackContainer">
+            {nameTec.map(({ name, ico }) => (
+              <div
+                className="div__stack"
+                key={name + ico}
+                style={{ marginRight: '10px' }}
+              >
+                <i className={`bx bxl-${ico}`} />
+                <span>{name}</span>
+              </div>
+            ))}
+          </div>
         </div>
-      </div>
-      <i className="bx bxl-django" />
-      <img src={imageProject} alt="Project" />
-    </article>
+        <i className="bx bxl-django" />
+        <img src={imageProject} alt="Project" />
+      </article>
+    </TiltWrapper>
   )
 }
 
@@ -43,7 +46,8 @@ Project.propTypes = {
   styleName: PropTypes.string.isRequired,
   textStyleName: PropTypes.string.isRequired,
   // eslint-disable-next-line react/forbid-prop-types
-  nameTec: PropTypes.array.isRequired
+  nameTec: PropTypes.array.isRequired,
+  imageProject: PropTypes.string.isRequired
 }
 
 export default Project
